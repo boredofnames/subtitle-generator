0141a0442bdbe831b260c51ead147b349585effd
Anthony K. <kilospade@gmail.com>
2024-08-22 21:56:33 -0500

    remove test.py

--- server/test.py
+++
@@ -1,4 +0,0 @@
-import hashlib
-url = "https://rumble.com/v5bf4nf-its-do-or-die-international-affairs-expert-on-putins-reaction-to-ukraines-k.html"
-filename = hashlib.shake_256(url.encode()).hexdigest(5)+".vtt"
-print(filename)
